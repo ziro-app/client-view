14b817d8d6f837f4cebe9ba59daa2b31bde685b9
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-05-17 20:38:17 -0700

    add header component

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ErrorBoundary from '../ErrorBoundary/index'
+import Header from '../Header/index'
 
-const App = () => <ErrorBoundary><div>Hi</div></ErrorBoundary>
+const App = () => <ErrorBoundary><Header title='Dados Clientes'>Hi</Header></ErrorBoundary>
 
 export default App
