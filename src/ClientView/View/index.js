ba401b8e05f06eeef0e5d325994fd2f7eea49d98
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-05-31 20:48:56 -0700

    add styles and basic structure for data list

--- src/ClientView/View/index.js
+++ src/ClientView/View/index.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Dropdown from '@ziro/dropdown'
+import { container, row, label, data } from './styles'
 
 const View = ({ state: { resellers, reseller }, updateDropdown }) => (
-	<Dropdown
-		name='reseller'
-		placeholder='Cliente'
-		options={resellers}
-		value={reseller}
-		updateParent={updateDropdown}
-	/>
+	<Fragment>
+		<Dropdown
+			name='reseller'
+			placeholder='Cliente'
+			options={resellers}
+			value={reseller}
+			updateParent={updateDropdown}
+		/>
+		<div style={container}>
+			{[{ value: 'test1', name: 'NAME1' },{ value: 'test2', name: 'NAME2' }].map( ({ value, name }) =>
+				<div style={row} key={name}>
+					<span style={label}>{name}</span>
+					<span style={data}>{value}</span>
+				</div>
+			)}
+		</div>
+	</Fragment>
 )
 
 View.propTypes = {
---
+++ src/ClientView/View/styles.js
@@ -0,0 +1,27 @@
+export const
+
+container = {
+	display: 'grid',
+	gridRowGap: '7px',
+	width: '100%',
+	textAlign: 'left',
+	fontSize: '1.5rem',
+	color: 'rgba(48, 62, 77, 0.8)'
+},
+
+row = {
+	display: 'grid',
+	gridTemplateColumns: '100px 1fr',
+	alignItems: 'center',
+	gridColumnGap: '5px',
+	overflow: 'hidden'
+},
+
+label = {
+	fontWeight: '600',
+	padding: '5px 0px'
+},
+
+data = {
+	padding: '5px 20px'
+}
